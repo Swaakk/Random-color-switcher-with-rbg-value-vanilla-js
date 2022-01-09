1a6a629abd00d73ce4dce9bf398dc0e1b3ca11ac
Swaakk <swathykrishna.thayyil@gmail.com>
2022-01-09 14:25:16 +0530

    added color change project

---
+++ main.js
@@ -0,0 +1,34 @@
+// immediatly invoked function expression
+
+
+(function(){
+
+    const colorField = document.querySelector(".color-field")
+    const switcher =  document.querySelector(".switcher")
+
+
+        function newColor(){
+            let red = Math.floor(Math.random() * 256)
+            let green = Math.floor(Math.random() * 256)
+            let blue = Math.floor(Math.random() * 256)
+            return `rgb(${red}, ${green}, ${blue})`
+        }
+
+
+
+        switcher.addEventListener("click", function(){
+            let color =  newColor()
+            document.body.style.background =  color
+            colorField.value = color
+        })
+})()
+
+
+
+
+
+function app(){
+    
+}
+
+
